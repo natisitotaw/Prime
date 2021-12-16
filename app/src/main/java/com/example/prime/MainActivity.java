c42eba722d0e53f915d60cad70444b38e9c435d4
nati stotaw <79826086+natisitotaw@users.noreply.github.com>
2021-12-16 03:47:10 -0600

    DEC 16

--- app/src/main/java/com/example/prime/MainActivity.java
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.prime;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseAuth auth;
+        FirebaseAuth mAuth;
+
+    }
+
+    public void Verify(View view) {
+
     }
 }
