dd085bbdd99e36ab47cd1b2d64e857ca33d8f012
nati stotaw <79826086+natisitotaw@users.noreply.github.com>
2021-12-28 12:01:43 -0600

    DEC 28

--- app/src/main/java/com/example/prime/Database/Userdb.java
+++ app/src/main/java/com/example/prime/Database/Userdb.java
@@ -1,6 +1,39 @@
 package com.example.prime.Database;
 
 public class Userdb {
+    String UserName, PhoneNO, Password;
 
+    public Userdb() {
 
+    }
+
+    public Userdb(String userName, String phoneNO, String password) {
+        UserName = userName;
+        PhoneNO = phoneNO;
+        Password = password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPhoneNO() {
+        return PhoneNO;
+    }
+
+    public void setPhoneNO(String phoneNO) {
+        PhoneNO = phoneNO;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
--- app/src/main/java/com/example/prime/Sign_Up.java
+++ app/src/main/java/com/example/prime/Sign_Up.java
@@ -9,11 +9,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.prime.Database.Userdb;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Sign_Up extends AppCompatActivity {
-    String phoneNo;
+    String phoneNo, password, username;
     EditText PhoneNo, Password, Username;
     FirebaseDatabase rootNode;
     DatabaseReference reference;
@@ -31,7 +32,13 @@ public class Sign_Up extends AppCompatActivity {
         //Toast.makeText(Sign_Up.this, PhoneNo.getText().toString().trim(), Toast.LENGTH_SHORT).show();
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("User");
-        reference.setValue("helliriro");
+
+        String username = Username.getText().toString();
+        String password = Password.getText().toString();
+        String phonenum = PhoneNo.getText().toString();
+
+        Userdb userdb = new Userdb(username, phonenum, password);
+        reference.child(phonenum).setValue(userdb);
 
         Intent intent = new Intent(getApplicationContext(), VerifyPhoneNo.class);
         intent.putExtra("PhoneNo", PhoneNo.getText().toString().trim());
