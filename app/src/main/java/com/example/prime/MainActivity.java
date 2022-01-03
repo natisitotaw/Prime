5c82090ff94fb33bf07bcb985fd2774def803778
nati stotaw <79826086+natisitotaw@users.noreply.github.com>
2022-01-03 06:48:18 -0600

    JAN 3

--- app/src/main/java/com/example/prime/MainActivity.java
+++ app/src/main/java/com/example/prime/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.prime;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,25 +16,70 @@ import android.os.Bundle;
 
 import android.provider.Settings;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.prime.Database.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
     private TextView create;
-
+    Button signin;
+    private EditText password,phoneno;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseAuth mAuth;
+        signin = (Button) findViewById(R.id.varify);
+        phoneno = (EditText) findViewById(R.id.userphoneno);
+        password = (EditText) findViewById(R.id.passwordentered);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        signin.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                ProgressDialog mDialog = new ProgressDialog(MainActivity.this);
+                mDialog.setMessage("Please Wait...");
+                mDialog.show();
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child(phoneno.getText().toString()).exists()){
+                            mDialog.dismiss();
+                            User user = snapshot.child(phoneno.getText().toString()).getValue(User.class);
+                            if(user.getPassword().equals(password.getText().toString())){
+                                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(MainActivity.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                            }
+                        }else{
+                            Toast.makeText(MainActivity.this, "User Doesn't Exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        });
     }
 
     public void SignIn(View view) {
@@ -42,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
             showCustomDialog();
         }
     }
+
 
     //Check internet
     private boolean isConnected(MainActivity mainActivity) {
